1357e344828f5e7b714fef30e2fd2a7116e557ef
000VIJAY <vijay893689@gmail.com>
2022-06-14 15:43:22 +0530

    added emloyee wage per day using switch statement

--- Program.cs
+++ Program.cs
@@ -2,31 +2,20 @@
 int employeeRatePerHr = 20;
 int employeeHrs = 0;
 int employeeWagePerDay = 0;
-int employeeWagePerMonth = 0;
-int maxWorkingDaysInMonth = 20;
+Random rnd = new Random();
+int randomCheck = rnd.Next(2);
 
-for (int i = 0; i <= maxWorkingDaysInMonth; i++)
+switch (randomCheck)
 {
-
-    Random rnd = new Random();
-    int employeeCheck = rnd.Next(0, 3);
-
-    switch (employeeCheck)
-    {
-        case 0:
-            employeeHrs = 8;
-            break;
-
-        case 1:
-            employeeHrs = 4;
-            break;
-
-        default:
-            employeeHrs = 0;
-            break;
-    }
-
-    employeeWagePerDay = employeeHrs * employeeRatePerHr;
-    employeeWagePerMonth += employeeWagePerDay;
-    Console.WriteLine("employeeWagePerDay : " + employeeWagePerMonth);
+    case 0:
+        employeeHrs = 8;
+        break;
+    case 1:
+        employeeHrs = 4;
+        break;
+    default:
+        employeeHrs = 0;
+        break;
 }
+employeeWagePerDay = employeeHrs * employeeRatePerHr;
+Console.WriteLine("employeeWage : " + employeeWagePerDay);

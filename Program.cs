21bf04aad778a04c9ab444f8fc530d9177212668
000VIJAY <vijay893689@gmail.com>
2022-06-14 15:07:18 +0530

    added isPresent

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Welcome to Employee Wage");
+Random rnd = new Random();
+int num = rnd.Next(0, 2);
+int isFullTime = 1;
+if (num == isFullTime)
+{
+    Console.WriteLine("Present");
+}
+else
+    Console.WriteLine("Absent");
